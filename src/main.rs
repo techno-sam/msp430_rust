2fbdbe3252448f32fe98e29c4c3ad7cd682acc79
techno-sam <77073745+techno-sam@users.noreply.github.com>
2023-10-11 21:54:33 -0700

    Add more license info

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+/*
+ *     MSP430 emulator
+ *     Copyright (C) 2023  Sam Wagenaar
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 use std::{time::Instant, fs::File, io::Read, sync::{Arc, atomic::{AtomicBool, Ordering}}};
 use libc::c_char;
 use std::ffi::CStr;
